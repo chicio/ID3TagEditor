2ce9b6d66761e2ede8bc4b83ea86b14006e16550
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-02-27 19:31:45 +0100

    Refactoring id3artworkframecreator :fire:

--- Source/Creation/Frame/Content/ID3ArtworkFrameCreator.swift
+++ Source/Creation/Frame/Content/ID3ArtworkFrameCreator.swift
@@ -11,6 +11,16 @@ class ID3ArtworkFrameCreator: ID3FrameCreatorsChain {
     private var id3FrameConfiguration: ID3FrameConfiguration
     private let frameContentSizeCalculator: FrameContentSizeCalculator
     private let frameFlagsCreator: FrameFlagsCreator
+    private let frameImageTypeHeader: [UInt8 : [String : [UInt8]]] = [
+        2: [
+            "png" : [0x00, 0x50, 0x4E, 0x47, 0x03, 0x00],
+            "jpeg" : [0x00, 0x4A, 0x50, 0x47, 0x03, 0x00]
+        ],
+        3 : [
+            "png" : [0x00, 0x69, 0x6D, 0x61, 0x67, 0x65, 0x2F, 0x70, 0x6E, 0x67, 0x00, 0x03, 0x00],
+            "jpeg" : [0x00, 0x69, 0x6D, 0x61, 0x67, 0x65, 0x2F, 0x6A, 0x70, 0x65, 0x67, 0x00, 0x03, 0x00]
+        ]
+    ]
 
     init(id3FrameConfiguration: ID3FrameConfiguration,
          frameContentSizeCalculator: FrameContentSizeCalculator,
@@ -23,29 +33,22 @@ class ID3ArtworkFrameCreator: ID3FrameCreatorsChain {
     override func createFrames(id3Tag: ID3Tag, tag: [UInt8]) -> [UInt8] {
         if let art = id3Tag.artwork.art {
             var frame: [UInt8] = id3FrameConfiguration.identifierFor(name: "artwork", version: id3Tag.version)
-
             var contentAsBytes: [UInt8] = [UInt8]()
-            if (id3Tag.artwork.isPNG!) {
-                if (id3Tag.version == 3) {
-                    contentAsBytes.append(contentsOf: [0x00, 0x69, 0x6D, 0x61, 0x67, 0x65, 0x2F, 0x70, 0x6E, 0x67, 0x00, 0x03, 0x00])
-                } else {
-                    contentAsBytes.append(contentsOf: [0x00, 0x50, 0x4E, 0x47, 0x03, 0x00])
-                }
-            } else {
-                if (id3Tag.version == 3) {
-                    contentAsBytes.append(contentsOf: [0x00, 0x69, 0x6D, 0x61, 0x67, 0x65, 0x2F, 0x6A, 0x70, 0x65, 0x67, 0x00, 0x03, 0x00]);
-                } else {
-                    contentAsBytes.append(contentsOf: [0x00, 0x4A, 0x50, 0x47, 0x03, 0x00])
-                }
-            }
-            let artworkData = [UInt8](art)
-            contentAsBytes.append(contentsOf: artworkData);
-
+            contentAsBytes.append(contentsOf: getImageTypeAndCoverTypeUsing(id3Tag: id3Tag))
+            contentAsBytes.append(contentsOf: [UInt8](art));
             frame.append(contentsOf: frameContentSizeCalculator.calculateSizeOf(content: contentAsBytes, version: id3Tag.version))
             frame.append(contentsOf: frameFlagsCreator.createFor(version: id3Tag.version))
             frame.append(contentsOf: contentAsBytes)
             return tag + frame
         }
         return super.createFrames(id3Tag: id3Tag, tag: tag)
+    }
+
+    private func getImageTypeAndCoverTypeUsing(id3Tag: ID3Tag) -> [UInt8] {
+        if id3Tag.artwork.isPNG! {
+            return frameImageTypeHeader[id3Tag.version]!["png"]!
+        } else {
+            return frameImageTypeHeader[id3Tag.version]!["jpeg"]!
+        }
     }
 }
