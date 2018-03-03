fce42047fee7e83a10ff491235c4fc45cbf4fda3
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-03-03 14:11:57 +0100

    Added package manager support :package:

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  ID3TagEditor
+//
+//  Created by Fabrizio Duroni on 03/03/2018.
+//  2018 Fabrizio Duroni
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "ID3TagEditor",
+    dependencies : [],
+    exclude: ["Tests"]
+)
