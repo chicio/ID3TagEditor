d38d9a0caaca0e73f673733cd1719a37e2d0f83f
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-05-01 23:52:19 +0200

    Added album artist frame :sparkles:

---
+++ Source/Creation/Frame/Content/ID3AlbumArtistFrameCreator.swift
@@ -0,0 +1,9 @@
+//
+//  ID3AlbumArtistFrameCreator.swift
+//  ID3TagEditor macOS
+//
+//  Created by Fabrizio Duroni on 01.05.18.
+//  Copyright © 2018 Fabrizio Duroni. All rights reserved.
+//
+
+import Foundation
