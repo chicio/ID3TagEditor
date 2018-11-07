d74b6117c0c74e48b2dfd00f24529887e5f91a83
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-11-07 21:24:06 +0100

    Fixed Package.swift for swift package manager :bug:

--- Package.swift
+++ Package.swift
@@ -1,15 +1,35 @@
-//
-//  Package.swift
-//  ID3TagEditor
-//
-//  Created by Fabrizio Duroni on 03/03/2018.
-//  2018 Fabrizio Duroni
-//
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "ID3TagEditor",
-    dependencies : [],
-    exclude: ["Tests"]
+    products: [
+        .library(
+            name: "ID3TagEditor",
+            targets: ["ID3TagEditor"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ID3TagEditor",
+            dependencies: [],
+            path: "./Source"
+        ),
+        .testTarget(
+            name: "ID3TagEditorTests",
+            dependencies: ["ID3TagEditor"],
+            path: "./Tests",
+            exclude: [
+                "Parsing/Frame/Content/Size/ID3FrameContentSizeParserTest.swift",
+                "Parsing/Frame/Size/ID3FrameSizeParserTest.swift",
+                "Parsing/Tag/Size/ID3TagSizeParserTest.swift",
+                "Parsing/Tag/Version/ID3TagVersionParserTest.swift",
+                "Acceptance/ID3TagEditorTestAcceptanceTest.swift",
+                "Mp3/Mp3FileReaderTest.swift"
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v4_2]
 )
