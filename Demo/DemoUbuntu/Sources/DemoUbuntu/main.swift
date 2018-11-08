bd22e16f07bf7968680605f388d50289c72da472
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-11-08 02:20:17 +0100

    Finish setup Demo Ubuntu :penguin:

--- Demo/DemoUbuntu/Sources/DemoUbuntu/main.swift
+++ Demo/DemoUbuntu/Sources/DemoUbuntu/main.swift
@@ -3,11 +3,18 @@ import ID3TagEditor
 
 print("Start...")
 
+let mp3Path = FileManager.default.currentDirectoryPath + "/Sources/DemoUbuntu/example-v3-additional-data.mp3"
+
 do {
     let id3TagEditor = ID3TagEditor()
-    if let id3Tag = try id3TagEditor.read(mp3: Data(contentsOf: URL(fileURLWithPath: "./example-v3-additional-data.mp3"))) {
-        print(id3Tag.title ?? "") 
-    } 
+    if let id3Tag = try id3TagEditor.read(mp3: Data(contentsOf: URL(fileURLWithPath: mp3Path))) {
+        print("Title: \(id3Tag.title ?? "")")
+        print("Artist: \(id3Tag.artist ?? "")")
+        print("Album: \(id3Tag.album ?? "")")
+        print("Album Artist: \(id3Tag.albumArtist ?? "")")
+        print("Genre: \(id3Tag.genre?.description ?? "")")
+        print("Year: \(id3Tag.recordingDateTime?.date?.year ?? 0)")
+    }
 } catch {
    print(error)
 }
