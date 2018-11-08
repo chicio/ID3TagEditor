a5fdbaaaef92c2204635dd197538478e0c24adea
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-11-08 01:33:56 +0100

    Removed corefoundation function again, incompatible with Ubuntu :bug:

--- Source/Parsing/Tag/Size/ID3TagSizeParser.swift
+++ Source/Parsing/Tag/Size/ID3TagSizeParser.swift
@@ -11,7 +11,7 @@ class ID3TagSizeParser: TagSizeParser {
     private let tagBytesOffset = 6;
     
     func parse(data: NSData) -> UInt32 {
-        let size = CFSwapInt32HostToBig((data.bytes + tagBytesOffset).assumingMemoryBound(to: UInt32.self).pointee);
+        let size = (data.bytes + tagBytesOffset).assumingMemoryBound(to: UInt32.self).pointee.bigEndian;
         let b1 = (size & 0x7F000000) >> 3;
         let b2 = (size & 0x007F0000) >> 2;
         let b3 = (size & 0x00007F00) >> 1;
