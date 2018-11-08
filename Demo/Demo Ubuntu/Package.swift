2e2a0943e40f4df9c867b863f3d829d0d9effd89
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-11-08 09:07:51 +0100

    Removed commas and formatted Package.swift in Demo Ubuntu :fire:

--- Demo/Demo Ubuntu/Package.swift
+++ Demo/Demo Ubuntu/Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Demo Ubuntu",
     dependencies: [
-        .package(url: "https://github.com/chicio/ID3TagEditor.git", from: "2.2.0"),
+        .package(url: "https://github.com/chicio/ID3TagEditor.git", from: "2.2.0")
     ],
     targets: [
         .target(
             name: "Demo Ubuntu",
-            dependencies: ["ID3TagEditor"]),
+            dependencies: ["ID3TagEditor"]
+        )
     ]
 )
