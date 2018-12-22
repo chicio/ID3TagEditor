5f6dff0dc798b663ac062fc961db1e33825225ce
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-12-22 17:28:04 +0100

    Refactoring: extracted ID3TagHeaderCreator :recycle:

--- Source/Creation/Tag/ID3TagCreator.swift
+++ Source/Creation/Tag/ID3TagCreator.swift
@@ -22,7 +22,7 @@ class ID3TagCreator {
 
     func create(id3Tag: ID3Tag) throws -> Data {
         let frames = try createFramesUsing(id3Tag: id3Tag)
-        let header = createTagHeader(contentSize: frames.count, id3Tag: id3Tag);
+        let header = ID3TagHeaderCreator(uInt32ToByteArrayAdapter: uInt32ToByteArrayAdapter, id3TagConfiguration: id3TagConfiguration).createTagHeader(contentSize: frames.count, id3Tag: id3Tag)
         let tag = header + frames
         if (isTooBig(tag: tag)) {
             throw ID3TagEditorError.TagTooBig
@@ -43,20 +43,31 @@ class ID3TagCreator {
         return frames
     }
 
-    private func createTagHeader(contentSize: Int, id3Tag: ID3Tag) -> [UInt8] {
-        id3Tag.properties.size = UInt32(contentSize)
-        let flags: [UInt8] = [0x00]
-        let header = id3TagConfiguration.headerFor(version: id3Tag.properties.version)
-            + flags
-            + uInt32ToByteArrayAdapter.adapt(uInt32: SynchsafeIntegerEncoder().encode(integer: id3Tag.properties.size))
-        return header
-    }
-
     private func createFramesEnd() -> [UInt8] {
         return [UInt8](repeating: 0, count: 2048)
     }
 
     private func isTooBig(tag: [UInt8]) -> Bool {
         return tag.count > 0xFFFFFFF
+    }
+}
+
+
+class ID3TagHeaderCreator {
+    private let uInt32ToByteArrayAdapter: UInt32ToByteArrayAdapter
+    private let id3TagConfiguration: ID3TagConfiguration
+    
+    init(uInt32ToByteArrayAdapter: UInt32ToByteArrayAdapter, id3TagConfiguration: ID3TagConfiguration) {
+        self.uInt32ToByteArrayAdapter = uInt32ToByteArrayAdapter
+        self.id3TagConfiguration = id3TagConfiguration
+    }
+    
+    func createTagHeader(contentSize: Int, id3Tag: ID3Tag) -> [UInt8] {
+        id3Tag.properties.size = UInt32(contentSize)
+        let flags: [UInt8] = [0x00]
+        let header = id3TagConfiguration.headerFor(version: id3Tag.properties.version)
+            + flags
+            + uInt32ToByteArrayAdapter.adapt(uInt32: SynchsafeIntegerEncoder().encode(integer: id3Tag.properties.size))
+        return header
     }
 }
