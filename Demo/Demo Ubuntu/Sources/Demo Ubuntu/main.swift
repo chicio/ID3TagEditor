137499a8f1c923dc888aed44431937d66244ef65
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-11-01 11:25:19 +0100

    Fix ubuntu demo optional chain :bug:

--- Demo/Demo Ubuntu/Sources/Demo Ubuntu/main.swift
+++ Demo/Demo Ubuntu/Sources/Demo Ubuntu/main.swift
@@ -8,10 +8,10 @@ let mp3Path = FileManager.default.currentDirectoryPath + "/Sources/Demo Ubuntu/e
 do {
     let id3TagEditor = ID3TagEditor()
     if let id3Tag = try id3TagEditor.read(mp3: Data(contentsOf: URL(fileURLWithPath: mp3Path))) {
-        print("Title: \((id3Tag?.frames[.Title] as? ID3FrameWithStringContent)?.content ?? "")")
-        print("Artist: \((id3Tag?.frames[.Artist] as? ID3FrameWithStringContent)?.content ?? "")")
-        print("Album: \((id3Tag?.frames[.Album] as? ID3FrameWithStringContent)?.content ?? "")")
-        print("Album Artist: \((id3Tag?.frames[.AlbumArtist] as? ID3FrameWithStringContent)?.content ?? "")")
+        print("Title: \((id3Tag.frames[.Title] as? ID3FrameWithStringContent)?.content ?? "")")
+        print("Artist: \((id3Tag.frames[.Artist] as? ID3FrameWithStringContent)?.content ?? "")")
+        print("Album: \((id3Tag.frames[.Album] as? ID3FrameWithStringContent)?.content ?? "")")
+        print("Album Artist: \((id3Tag.frames[.AlbumArtist] as? ID3FrameWithStringContent)?.content ?? "")")
     }
 } catch {
    print(error)
