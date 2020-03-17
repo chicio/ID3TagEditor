1cddbdd4c771530a7e9a3d5576eeff9f0e27f40e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-17 14:49:34 -0600

    Fixed test resource loading.

--- Tests/Utils/PathLoader.swift
+++ Tests/Utils/PathLoader.swift
@@ -9,8 +9,18 @@ import Foundation
 
 class PathLoader {
     func pathFor(name: String, fileType: String) -> String {
+        #if SWIFT_PACKAGE
+        // Will need to switch to “#filePath” after SE‐0274:
+        // https://github.com/apple/swift-evolution/blob/master/proposals/0274-magic-file.md
+        let thisFile = URL(fileURLWithPath: #file)
+        let testsDirectory = thisFile.deletingLastPathComponent().deletingLastPathComponent()
+        let examples = testsDirectory.appendingPathComponent("Examples")
+        let url = examples.appendingPathComponent(name).appendingPathExtension(fileType)
+        let path = url.path
+        #else
         let bundle = Bundle(for: type(of: self));
         let path = bundle.path(forResource: name, ofType: fileType)!;
+        #endif
         return path;
     }
 }
