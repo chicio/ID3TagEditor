881296f5e2a9553356d356abdd639e1030ac5f5c
Nolaine Crusher <nolainecrusher@gmail.com>
2020-03-30 16:30:19 -0700

    trying to add ascii string to byte adapter. Not sure I'm doing it right.

---
+++ Source/Creation/Frame/Content/String/Encoding/ID3ASCIIStringToByteAdapter.swift
@@ -0,0 +1,33 @@
+//
+//  ID3ASCIIStringToByteAdapter.swift
+//
+//  Created by Fabrizio Duroni on 27/04/2018.
+//  2018 Fabrizio Duroni.
+//
+
+import Foundation
+
+class ID3ASCIIStringToByteAdapter: StringToBytesAdapter {
+    private let paddingAdder: PaddingAdder
+    private let frameConfiguration: ID3FrameConfiguration
+    
+    init(paddingAdder: PaddingAdder, frameConfiguration: ID3FrameConfiguration) {
+        self.paddingAdder = paddingAdder
+        self.frameConfiguration = frameConfiguration
+    }
+
+    func encoding(for version: ID3Version) -> [UInt8] {
+        return frameConfiguration.encodingByteFor(version: version, encoding: .ascii)
+    }
+
+    func termination() -> [UInt8] {
+      return [UInt8](repeating: 0, count: 1)
+    }
+
+    func adapt(stringOnly string: String) -> [UInt8] {
+      #warning("Shouldn’t this throw instead?")
+      // If the string is not actually representable in ISO 8859‐1, falling back to UTF‐8 enables any ASCII portions to remain legible, but will garble anything else. (It will be “valid” gibberish.)
+        return string.data(using: .ascii).map({ [UInt8]($0) })
+        ?? [UInt8](string.utf8)
+    }
+}
