462ab1ab985ff62216c037d36902a018d3c73bbd
fduroni <fabrizio.duroni@gmail.com>
2020-10-26 13:42:38 +0100

    Added last method doc for builders :sparkles:

--- Source/Tag/FileOwnerFrameBuilder.swift
+++ Source/Tag/FileOwnerFrameBuilder.swift
@@ -10,10 +10,17 @@ import Foundation
 
 /**
  Protocol adopted by ID3Tag builders that support the .fileOwner (TOWN) frame. At the
- moment of this writing this frame is supported by ID3Tag version 3 and version 4 support .
+ moment of this writing this frame is supported by ID3Tag version 3 and version 4.
  You should not adopt/use this protocol. Use ID32v4TagBuilder to create a ID3Tag version 4.
  */
 public protocol FileOwnerFrameBuilder where Self: ID32TagCommonStringFramesBuilder {
+    /**
+      Set the fileOwner frame to be written by ID3TagEditor.
+     
+      - parameter frame: the fileOwner frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func fileOwner(frame: ID3FrameWithStringContent) -> Self
 }
 
@@ -22,6 +29,13 @@ public protocol FileOwnerFrameBuilder where Self: ID32TagCommonStringFramesBuild
  FileOwnerFrameBuilder set the .fileOwner (TOWN) frame.
  */
 public extension FileOwnerFrameBuilder {
+    /**
+      Set the fileOwner frame to be written by ID3TagEditor.
+     
+      - parameter frame: the fileOwner frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func fileOwner(frame: ID3FrameWithStringContent) -> Self {
         frames[.fileOwner] = frame
         return self
--- Source/Tag/ITunesFramesBuilder.swift
+++ Source/Tag/ITunesFramesBuilder.swift
@@ -14,13 +14,76 @@ import Foundation
  Use ID32v3TagBuilder and ID32v4TagBuilder to create an ID3Tag with this frames.
  */
 public protocol ITunesFramesBuilder where Self: ID32TagCommonStringFramesBuilder {
+    /**
+      Set the iTunesGrouping frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesGrouping frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesGrouping(frame: ID3FrameWithStringContent) -> Self
+
+    /**
+      Set the iTunesGrouping frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesMovementName frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesMovementName(frame: ID3FrameWithStringContent) -> Self
+
+    /**
+      Set the iTunesMovementIndex frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesMovementIndex frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesMovementIndex(frame: ID3FrameWithIntegerContent) -> Self
+
+    /**
+      Set the iTunesMovementCount frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesMovementCount frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesMovementCount(frame: ID3FrameWithIntegerContent) -> Self
+
+    /**
+      Set the iTunesPodcastCategory frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastCategory frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastCategory(frame: ID3FrameWithStringContent) -> Self
+
+    /**
+      Set the iTunesPodcastDescription frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastDescription frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastDescription(frame: ID3FrameWithStringContent) -> Self
+
+    /**
+      Set the iTunesPodcastID frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastID frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastID(frame: ID3FrameWithStringContent) -> Self
+
+    /**
+      Set the iTunesPodcastKeywords frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastKeywords frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastKeywords(frame: ID3FrameWithStringContent) -> Self
 }
 
@@ -29,41 +92,97 @@ public protocol ITunesFramesBuilder where Self: ID32TagCommonStringFramesBuilder
  ITunesFramesBuilder set unofficial iTunes ad-hoc frames.
  */
 public extension ITunesFramesBuilder {
+    /**
+      Set the iTunesGrouping frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesGrouping frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesGrouping(frame: ID3FrameWithStringContent) -> Self {
         frames[.iTunesGrouping] = frame
         return self
     }
 
+    /**
+      Set the iTunesGrouping frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesMovementName frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesMovementName(frame: ID3FrameWithStringContent) -> Self {
         frames[.iTunesMovementName] = frame
         return self
     }
 
+    /**
+      Set the iTunesMovementIndex frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesMovementIndex frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesMovementIndex(frame: ID3FrameWithIntegerContent) -> Self {
         frames[.iTunesMovementIndex] = frame
         return self
     }
 
+    /**
+      Set the iTunesMovementCount frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesMovementCount frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesMovementCount(frame: ID3FrameWithIntegerContent) -> Self {
         frames[.iTunesMovementCount] = frame
         return self
     }
 
+    /**
+      Set the iTunesPodcastCategory frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastCategory frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastCategory(frame: ID3FrameWithStringContent) -> Self {
         frames[.iTunesPodcastCategory] = frame
         return self
     }
 
+    /**
+      Set the iTunesPodcastDescription frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastDescription frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastDescription(frame: ID3FrameWithStringContent) -> Self {
         frames[.iTunesPodcastDescription] = frame
         return self
     }
 
+    /**
+      Set the iTunesPodcastID frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastID frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastID(frame: ID3FrameWithStringContent) -> Self {
         frames[.iTunesPodcastID] = frame
         return self
     }
 
+    /**
+      Set the iTunesPodcastKeywords frame to be written by ID3TagEditor.
+     
+      - parameter frame: the iTunesPodcastKeywords frame as a ID3FrameWithStringContent instance.
+     
+      - returns: the instance of the builder.
+     */
     func iTunesPodcastKeywords(frame: ID3FrameWithStringContent) -> Self {
         frames[.iTunesPodcastKeywords] = frame
         return self
