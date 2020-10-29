d6a5e8de2aca9d24fc704231ec5d9c246b5dbc80
fduroni <fabrizio.duroni@gmail.com>
2020-10-29 21:51:17 +0100

    Extracted id3tag description :recycle:

--- Source/Tag/ID3Tag.swift
+++ Source/Tag/ID3Tag.swift
@@ -5,8 +5,6 @@
 //  2018 Fabrizio Duroni.
 //
 
-// swiftlint:disable line_length
-
 import Foundation
 
 /**
@@ -21,57 +19,7 @@ public class ID3Tag: CustomDebugStringConvertible {
     }()
     /// ID3Tag description, useful for debug.
     public var debugDescription: String {
-        return """
-        ID3Tag:
-        - size: \(self.properties.size)
-        - version: \(self.properties.version)
-        - artist: \((self.frames[.artist] as? ID3FrameWithStringContent)?.content ?? "-")
-        - composer: \((self.frames[.composer] as? ID3FrameWithStringContent)?.content ?? "-")
-        - conductor: \((self.frames[.conductor] as? ID3FrameWithStringContent)?.content ?? "-")
-        - contentGrouping: \((self.frames[.contentGrouping] as? ID3FrameWithStringContent)?.content ?? "-")
-        - copyright: \((self.frames[.copyright] as? ID3FrameWithStringContent)?.content ?? "-")
-        - encodedBy: \((self.frames[.encodedBy] as? ID3FrameWithStringContent)?.content ?? "-")
-        - encoderSettings: \((self.frames[.encoderSettings] as? ID3FrameWithStringContent)?.content ?? "-")
-        - fileOwner: \((self.frames[.fileOwner] as? ID3FrameWithStringContent)?.content ?? "-")
-        - iTunesGrouping: \((self.frames[.iTunesGrouping] as? ID3FrameWithStringContent)?.content ?? "-")
-        - lyricist: \((self.frames[.lyricist] as? ID3FrameWithStringContent)?.content ?? "-")
-        - mixArtist: \((self.frames[.mixArtist] as? ID3FrameWithStringContent)?.content ?? "-")
-        - iTunesMovementIndex: \((self.frames[.iTunesMovementIndex] as? ID3FrameWithIntegerContent)?.debugDescription ?? "-")
-        - iTunesMovementCount: \((self.frames[.iTunesMovementCount] as? ID3FrameWithIntegerContent)?.debugDescription ?? "-")
-        - iTunesMovementName: \((self.frames[.iTunesMovementName] as? ID3FrameWithStringContent)?.content ?? "-")
-        - podcastCategory: \((self.frames[.iTunesPodcastCategory] as? ID3FrameWithStringContent)?.content ?? "-")
-        - podcastDescription: \((self.frames[.iTunesPodcastDescription] as? ID3FrameWithStringContent)?.content ?? "-")
-        - podcastID: \((self.frames[.iTunesPodcastID] as? ID3FrameWithStringContent)?.content ?? "-")
-        - podcastKeywords: \((self.frames[.iTunesPodcastKeywords] as? ID3FrameWithStringContent)?.content ?? "-")
-        - publisher: \((self.frames[.publisher] as? ID3FrameWithStringContent)?.content ?? "-")
-        - subtitle: \((self.frames[.subtitle] as? ID3FrameWithStringContent)?.content ?? "-")
-        - albumArtist: \((self.frames[.albumArtist] as? ID3FrameWithStringContent)?.content ?? "-")
-        - title: \((self.frames[.title] as? ID3FrameWithStringContent)?.content ?? "-")
-        - trackPosition: \((self.frames[.trackPosition] as? ID3FramePartOfTotal)?.debugDescription ?? "-")
-        - discPosition: \((self.frames[.discPosition] as? ID3FramePartOfTotal)?.debugDescription ?? "-")
-        - album: \((self.frames[.album] as? ID3FrameWithStringContent)?.content ?? "-")
-        - recordingDateTime: \(
-            (self.frames[.recordingDateTime] as? ID3FrameRecordingDateTime)?.recordingDateTime.debugDescription ?? "-"
-        )
-        - recordingYear: \((self.frames[.recordingYear] as? ID3FrameRecordingYear)?.debugDescription ?? "-")
-        - recordingDayMonth: \((self.frames[.recordingDayMonth] as? ID3FrameRecordingDayMonth)?.debugDescription ?? "-")
-        - recordingHourMinute: \((self.frames[.recordingHourMinute] as? ID3FrameRecordingHourMinute)?.debugDescription ?? "-")
-        - genre: \((self.frames[.genre] as? ID3FrameGenre)?.debugDescription ?? "-")
-        - attachedPicture:\(ID3PictureType.allCases.reduce("", { (accumulator: String, current: ID3PictureType) in
-            if let frameDescription = (self.frames[.attachedPicture(current)] as? ID3FrameAttachedPicture)?.debugDescription {
-                return accumulator + "\n" + frameDescription + "\n"
-            }
-
-            return accumulator
-        }))
-        - unsynchronisedLyrics:\(ID3FrameContentLanguage.allCases.reduce("", { (accumulator: String, current: ID3FrameContentLanguage) in
-            if let frameDescription = (self.frames[.unsynchronizedLyrics(current)] as? ID3FrameWithLocalizedContent)?.debugDescription {
-                return accumulator + "\n" + frameDescription + "\n"
-            }
-
-            return accumulator
-        }))
-        """
+        return ID3TagToStringAdapter().adapt(id3Tag: self)
     }
 
     init(version: ID3Version, frames: [FrameName: ID3Frame]) {
---
+++ Source/Tag/ID3TagToStringAdapter.swift
@@ -0,0 +1,78 @@
+//
+//  ID3TagToStringAdapter.swift
+//  ID3TagEditor
+//
+//  Created by Fabrizio Duroni on 29.10.20.
+//  2020 Fabrizio Duroni.
+//
+
+// swiftlint:disable line_length
+// swiftlint:disable function_body_length
+
+import Foundation
+
+class ID3TagToStringAdapter {
+    func adapt(id3Tag: ID3Tag) -> String {
+        return """
+        ID3Tag:
+        - size: \(id3Tag.properties.size)
+        - version: \(id3Tag.properties.version)
+        - artist: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .artist))
+        - composer: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .composer))
+        - conductor: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .conductor))
+        - contentGrouping: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .contentGrouping))
+        - copyright: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .copyright))
+        - encodedBy: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .encodedBy))
+        - encoderSettings: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .encoderSettings))
+        - fileOwner: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .fileOwner))
+        - lyricist: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .lyricist))
+        - mixArtist: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .mixArtist))
+        - publisher: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .publisher))
+        - subtitle: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .subtitle))
+        - albumArtist: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .albumArtist))
+        - title: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .title))
+        - trackPosition: \((id3Tag.frames[.trackPosition] as? ID3FramePartOfTotal)?.debugDescription ?? "-")
+        - discPosition: \((id3Tag.frames[.discPosition] as? ID3FramePartOfTotal)?.debugDescription ?? "-")
+        - album: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .album))
+        - recordingDateTime: \(
+            (id3Tag.frames[.recordingDateTime] as? ID3FrameRecordingDateTime)?.recordingDateTime.debugDescription ?? "-"
+        )
+        - recordingYear: \((id3Tag.frames[.recordingYear] as? ID3FrameRecordingYear)?.debugDescription ?? "-")
+        - recordingDayMonth: \((id3Tag.frames[.recordingDayMonth] as? ID3FrameRecordingDayMonth)?.debugDescription ?? "-")
+        - recordingHourMinute: \((id3Tag.frames[.recordingHourMinute] as? ID3FrameRecordingHourMinute)?.debugDescription ?? "-")
+        - genre: \((id3Tag.frames[.genre] as? ID3FrameGenre)?.debugDescription ?? "-")
+        - attachedPicture:\(ID3PictureType.allCases.reduce("", { (accumulator: String, current: ID3PictureType) in
+            if let frameDescription = (id3Tag.frames[.attachedPicture(current)] as? ID3FrameAttachedPicture)?.debugDescription {
+                return accumulator + "\n" + frameDescription + "\n"
+            }
+
+            return accumulator
+        }))
+        - unsynchronisedLyrics:\(adaptFrameWithLocalizedContent(id3Tag: id3Tag, frameName: FrameName.unsynchronizedLyrics))
+        - comments:\(adaptFrameWithLocalizedContent(id3Tag: id3Tag, frameName: FrameName.comment))
+        - iTunesGrouping: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesGrouping))
+        - iTunesMovementIndex: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesMovementIndex))
+        - iTunesMovementCount: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesMovementCount))
+        - iTunesMovementName: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesMovementName))
+        - iTunesGrouping: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesGrouping))
+        - iTunesPodcastCategory: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesPodcastCategory))
+        - iTunesPodcastDescription: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesPodcastDescription))
+        - iTunesPodcastID: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesPodcastKeywords))
+        - iTunesPodcastKeyword: \(adaptFrameWithStringContentFrom(id3Tag: id3Tag, frameName: .iTunesPodcastKeywords))
+        """
+    }
+
+    private func adaptFrameWithStringContentFrom(id3Tag: ID3Tag, frameName: FrameName) -> String {
+        return (id3Tag.frames[frameName] as? ID3FrameWithIntegerContent)?.debugDescription ?? "-"
+    }
+
+    private func adaptFrameWithLocalizedContent(id3Tag: ID3Tag, frameName: (ID3FrameContentLanguage) -> FrameName) -> String {
+        return ID3FrameContentLanguage.allCases.reduce("", { (accumulator: String, current: ID3FrameContentLanguage) in
+            if let frameDescription = (id3Tag.frames[frameName(current)] as? ID3FrameWithLocalizedContent)?.debugDescription {
+                return accumulator + "\n" + frameDescription + "\n"
+            }
+
+            return accumulator
+        })
+    }
+}
