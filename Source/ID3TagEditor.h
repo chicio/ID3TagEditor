21e3c5cf154e50dace89de8c32f2c020821a21ef
Shabinder Singh <dev.shabinder@gmail.com>
2021-04-25 23:36:19 +0530

    Replace @import (Not Compatible with KMP)

--- Source/ID3TagEditor.h
+++ Source/ID3TagEditor.h
@@ -5,7 +5,7 @@
 //  2018 Fabrizio Duroni.
 //
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT double ID3TagEditorVersionNumber;
 FOUNDATION_EXPORT const unsigned char ID3TagEditorVersionString[];
