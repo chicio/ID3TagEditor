e68149a3bbb3858cd80fb3d9cc037a153f952559
Zsolt Kovacs <lordzsoltt@gmail.com>
2023-01-06 00:56:47 +0100

    Add optimized reader that only read the ID3 header of the file

--- Tests/Mp3/Mp3FileReaderTest.swift
+++ Tests/Mp3/Mp3FileReaderTest.swift
@@ -11,16 +11,18 @@ import XCTest
 class Mp3FileReaderTest: XCTestCase {
     func testNotAnMp3file() {
         let path = PathLoader().pathFor(name: "example-cover", fileType: "jpg")
-        let mp3FileReader = Mp3FileReader()
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
 
-        XCTAssertThrowsError(try mp3FileReader.readFrom(path: path))
+        XCTAssertThrowsError(try mp3FileReader.readFileFrom(path: path))
     }
 
     func testMp3File() {
         let path = PathLoader().pathFor(name: "example", fileType: "mp3")
-        let mp3FileReader = Mp3FileReader()
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
 
-        XCTAssertNoThrow(try mp3FileReader.readFrom(path: path))
+        XCTAssertNoThrow(try mp3FileReader.readFileFrom(path: path))
     }
 
     static let allTests = [
