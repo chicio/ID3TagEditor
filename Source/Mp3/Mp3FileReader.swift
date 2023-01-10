5d345d644f2ca21a6b6a387de790127b45dd657d
Zsolt Kovács <lordzsoltt@gmail.com>
2023-01-10 14:09:50 +0100

    Add optimized reader that only read the ID3 header of the file (#99)
    * Add optimized reader that only read the ID3 header of the file
    
    * Modify ID3TagEditor to use the optimized reader
    
    when reading header, and use the full file reader
    when saving file
    
    * Add tests
    
    * Update build-watchos.yml
    
    * Update build-watchos.yml
    
    * Apply code review changes
    
    - Fix naming
    - Create FileReaderFactory
    
    Co-authored-by: Fabrizio Duroni <fabrizio.duroni@gmail.com>

--- Source/Mp3/Mp3FileReader.swift
+++ Source/Mp3/Mp3FileReader.swift
@@ -8,12 +8,73 @@
 import Foundation
 
 class Mp3FileReader {
-    func readFrom(path: String) throws -> Data {
+    private let tagSizeParser: TagSizeParser
+    private let id3TagConfiguration: ID3TagConfiguration
+
+    init(tagSizeParser: TagSizeParser,
+         id3TagConfiguration: ID3TagConfiguration) {
+        self.tagSizeParser = tagSizeParser
+        self.id3TagConfiguration = id3TagConfiguration
+    }
+
+    /**
+      Read the entire mp3 file at path
+
+      - parameter path: the path to the mp3 file
+
+      - returns: mp3 data of the file
+
+      - throws: Could throw `InvalidFileFormat` if an mp3 file doesn't exists at the specified path.
+     */
+    func readFileFrom(path: String) throws -> Data {
         let validPath = URL(fileURLWithPath: path)
         guard validPath.pathExtension.caseInsensitiveCompare("mp3") == ComparisonResult.orderedSame else {
             throw ID3TagEditorError.invalidFileFormat
         }
+
         let mp3 = try Data(contentsOf: validPath)
         return mp3
+    }
+
+    /**
+      Read only the ID3 header of the mp3 file at path
+
+      - parameter path: the path to the mp3 file
+
+      - returns: ID3 header data of the file
+
+      - throws: Could throw `InvalidFileFormat` if an mp3 file doesn't exists at the specified path, or if the file
+     does not contain the entire ID3 header
+     */
+    func readID3TagFrom(path: String) throws -> Data {
+        let validPath = URL(fileURLWithPath: path)
+        guard validPath.pathExtension.caseInsensitiveCompare("mp3") == ComparisonResult.orderedSame else {
+            throw ID3TagEditorError.invalidFileFormat
+        }
+
+        guard let inputStream = InputStream(fileAtPath: path) else {
+            throw ID3TagEditorError.corruptedFile
+        }
+
+        inputStream.open()
+
+        let headerSize = id3TagConfiguration.headerSize()
+        let header = try read(bytesCount: headerSize, fromStream: inputStream)
+        let headerData = Data(header) as NSData
+
+        let frameSize = tagSizeParser.parse(data: headerData)
+        let frame = try read(bytesCount: Int(frameSize), fromStream: inputStream)
+
+        let mp3 = header + frame
+        return Data(mp3)
+    }
+
+    private func read(bytesCount: Int, fromStream stream: InputStream) throws -> [UInt8] {
+        var buffer = [UInt8](repeating: 0, count: bytesCount)
+        let result = stream.read(&buffer, maxLength: bytesCount)
+        if result < bytesCount {
+            throw ID3TagEditorError.corruptedFile
+        }
+        return buffer
     }
 }
--- Tests/Mp3/Mp3FileReaderTest.swift
+++ Tests/Mp3/Mp3FileReaderTest.swift
@@ -9,22 +9,63 @@ import XCTest
 @testable import ID3TagEditor
 
 class Mp3FileReaderTest: XCTestCase {
-    func testNotAnMp3file() {
+    func testNotAnMP3FileWhenReadingEntireFile() {
         let path = PathLoader().pathFor(name: "example-cover", fileType: "jpg")
-        let mp3FileReader = Mp3FileReader()
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
 
-        XCTAssertThrowsError(try mp3FileReader.readFrom(path: path))
+        XCTAssertThrowsError(try mp3FileReader.readFileFrom(path: path))
     }
 
-    func testMp3File() {
+    func testMP3FileWhenReadingEntireFile() {
         let path = PathLoader().pathFor(name: "example", fileType: "mp3")
-        let mp3FileReader = Mp3FileReader()
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
 
-        XCTAssertNoThrow(try mp3FileReader.readFrom(path: path))
+        XCTAssertNoThrow(try mp3FileReader.readFileFrom(path: path))
+    }
+
+    func testNotAnMP3fileWhenReadingID3Tag() {
+        let path = PathLoader().pathFor(name: "example-cover", fileType: "jpg")
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
+
+        XCTAssertThrowsError(try mp3FileReader.readID3TagFrom(path: path))
+    }
+
+    func testMP3fileWhenReadingID3Tag() {
+        let path = PathLoader().pathFor(name: "example", fileType: "mp3")
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
+
+        XCTAssertNoThrow(try mp3FileReader.readID3TagFrom(path: path))
+    }
+
+    func testNonExistentMP3fileWhenReadingID3Tag() {
+        let path = "/non-existent.mp3"
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
+
+        XCTAssertThrowsError(try mp3FileReader.readID3TagFrom(path: path))
+    }
+
+    func testOnlyReadsID3Tag() throws {
+        let path = PathLoader().pathFor(name: "example", fileType: "mp3")
+        let mp3FileReader = Mp3FileReader(tagSizeParser: ID3TagSizeParser(),
+                                          id3TagConfiguration: ID3TagConfiguration())
+
+        let id3TagData = try mp3FileReader.readID3TagFrom(path: path)
+
+        // 10 bytes Tag + 34213 bytes according to the Tag Size in the file's ID3 Tag
+        XCTAssertEqual(id3TagData.count, 10 + 34213)
     }
 
     static let allTests = [
-        ("testNotAnMp3file", testNotAnMp3file),
-        ("testMp3File", testMp3File)
+        ("testNotAnMP3FileWhenReadingEntireFile", testNotAnMP3FileWhenReadingEntireFile),
+        ("testMP3FileWhenReadingEntireFile", testMP3FileWhenReadingEntireFile),
+        ("testNotAnMP3fileWhenReadingID3Tag", testNotAnMP3fileWhenReadingID3Tag),
+        ("testMP3fileWhenReadingID3Tag", testMP3fileWhenReadingID3Tag),
+        ("testNonExistentMP3fileWhenReadingID3Tag", testNonExistentMP3fileWhenReadingID3Tag),
+        ("testOnlyReadsID3Tag", testOnlyReadsID3Tag)
     ]
 }
