f289d3457fb7129889dc2696375d115851908587
Oleg Müller <mail@bitgrainedbytes.com>
2023-04-18 21:59:41 +0200

    swiftlint fixes

--- Tests/Parse/ID3StringContentParsingOperationTest.swift
+++ Tests/Parse/ID3StringContentParsingOperationTest.swift
@@ -17,7 +17,7 @@ class ID3StringContentParsingOperationTest: XCTestCase {
         paddingRemover: PaddingRemoverUsingTrimming(),
         id3FrameConfiguration: ID3FrameConfiguration()
     )
-    
+
     func testTooSmallDataValue() {
         let value = stringContentParser.parse(frame: Data(capacity: 1), version: .version3)
         XCTAssertNil(value)
