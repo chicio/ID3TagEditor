0f411f8aec8c31b312c68d53e3f4548cc11c5166
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2024-10-13 13:18:48 +0200

    Adding host base path :bulb:

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -10,6 +10,7 @@ swift package --allow-writing-to-directory docs \
     generate-documentation --target ID3TagEditor \
     --disable-indexing \
     --transform-for-static-hosting \
+    --hosting-base-path ID3TagEditor \
     --output-path docs
 
-echo "id3tageditor.fabrizioduroni.it" > "docs/CNAME" 
+echo "id3tageditor.fabrizioduroni.it" > "docs/CNAME"
