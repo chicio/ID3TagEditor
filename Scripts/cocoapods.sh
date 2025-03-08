870519da9f7a398a25c98c640a7edfabf0013300
chicio <fabrizio.duroni@gmail.com>
2025-03-08 10:05:20 +0100

    Added go to root folder for scripts :rocket:

--- Scripts/cocoapods.sh
+++ Scripts/cocoapods.sh
@@ -6,6 +6,11 @@
 #  Created by Fabrizio Duroni on 08/03/25.
 #  Copyright © 2025 Fabrizio Duroni. All rights reserved.
 
+# Get root folder of the Git repository
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd "$GIT_ROOT" || { echo "Failed to change directory to project root."; exit 1; }
+
 # Publish release on cocoapods
 
 echo "Publish release on cocoapods..."
--- Scripts/docs.sh
+++ Scripts/docs.sh
@@ -6,6 +6,11 @@
 #  Created by Fabrizio Duroni on 10.10.18.
 #  2020 Fabrizio Duroni.
 
+# Get root folder of the Git repository
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd "$GIT_ROOT" || { echo "Failed to change directory to project root."; exit 1; }
+
 echo "Generating docs..."
 
 swift package --allow-writing-to-directory docs \
